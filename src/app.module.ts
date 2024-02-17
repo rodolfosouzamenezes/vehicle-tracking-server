dabc545363b513659462df0fa794ea6c7754cdb0
rodolfosouzamenezes <rodolfosouzaofc@gmail.com>
2024-02-17 19:48:15 +0000

    feat: get place_id by text

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { RoutesModule } from './routes/routes.module';
+import { ConfigModule } from '@nestjs/config';
+import { MapsModule } from './maps/maps.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    PrismaModule, 
+    RoutesModule, 
+    MapsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
